2eeba48ef076e196ed2c9f2bc5551b4bd94c4900
ethankclam <ethankclam@gmail.com>
2018-09-01 16:23:50 +0800

    the where statement

---
+++ where.sql
@@ -0,0 +1,21 @@
+SELECT * FROM customers
+WHERE ID = 7;
+
+SELECT * FROM customers
+WHERE ID BETWEEN 3 AND 7;
+
+SELECT * FROM customers
+WHERE city = 'Boston';
+
+SELECT * FROM customers
+WHERE city = 'Boston' AND Age <= 40;
+
+SELECT * FROM customers
+WHERE city = 'Boston'
+AND (Age >= 30 OR Age <= 40);
+
+SELECT * FROM customers
+WHERE City IN ('New York', 'Los Angeles', 'Chicago');
+
+SELECT CONTACT(FirstName, ',', City)
+FROM customers;
